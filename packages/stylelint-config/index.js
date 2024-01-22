0c6b932f6348463fc03b39b193de25e413b2dc04
Simon Gaudreau <sgaudreau@exolnet.com>
2024-01-22 16:11:44 -0500

    fix: remove deprecated stylelint rule

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -2,7 +2,6 @@ module.exports = {
     'extends': 'stylelint-config-standard-scss',
     'rules': {
         'at-rule-no-unknown': null,
-        'indentation': 4,
         'no-descending-specificity': null,
         'scss/at-rule-no-unknown': true,
         'selector-class-pattern': null,
