c69d632fbe5100d2373c54e3e1f35812c3a206af
Simon Gaudreau <sgaudreau@exolnet.com>
2024-04-26 13:06:58 -0400

    feat: migrate eslint-config-base to eslint 9.x flat config

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,38 +1,43 @@
-module.exports = {
-    extends: [
-        'eslint:recommended',
-    ],
+const js = require('@eslint/js');
+const esLintGlobals = require('globals');
 
-    env: {
-        es6: true,
-        browser: true,
-        node: true,
-    },
+module.exports = [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...esLintGlobals.node,
+                ...Object.keys(esLintGlobals.browser).filter((objKey)=>{
+                    objKey !== 'AudioWorkletGlobalScope ';
+                }),
+            },
+        },
 
-    rules: {
-        'array-bracket-spacing': ['error', 'never'],
-        'brace-style': ['error', '1tbs'],
-        'comma-dangle': ['error', 'always-multiline'],
-        'comma-style': ['error', 'last'],
-        'eqeqeq': ['error', 'always'],
-        'indent': ['error', 4, {
-            'SwitchCase': 1,
-        }],
-        'linebreak-style': ['error', 'unix'],
-        'keyword-spacing': 'error',
-        'prefer-const': 'error',
-        'new-parens': 'error',
-        'no-case-declarations': 'off',
-        'no-const-assign': 'error',
-        'no-unused-vars': ['error', { 'args': 'none' }],
-        'no-whitespace-before-property': 'error',
-        'object-curly-spacing': ['error', 'always'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'space-before-blocks': 'error',
-        'space-before-function-paren': ['error', { 'anonymous': 'always', 'named': 'never' }],
-        'space-in-parens': ['error', 'never'],
-        'space-infix-ops': 'error',
-        'strict': ['error', 'never'],
+        rules: {
+            'array-bracket-spacing': ['error', 'never'],
+            'brace-style': ['error', '1tbs'],
+            'comma-dangle': ['error', 'always-multiline'],
+            'comma-style': ['error', 'last'],
+            'eqeqeq': ['error', 'always'],
+            'indent': ['error', 4, {
+                'SwitchCase': 1,
+            }],
+            'linebreak-style': ['error', 'unix'],
+            'keyword-spacing': 'error',
+            'prefer-const': 'error',
+            'new-parens': 'error',
+            'no-case-declarations': 'off',
+            'no-const-assign': 'error',
+            'no-unused-vars': ['error', { 'args': 'none' }],
+            'no-whitespace-before-property': 'error',
+            'object-curly-spacing': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'space-before-blocks': 'error',
+            'space-before-function-paren': ['error', { 'anonymous': 'always', 'named': 'never' }],
+            'space-in-parens': ['error', 'never'],
+            'space-infix-ops': 'error',
+            'strict': ['error', 'never'],
+        },
     },
-};
+];
