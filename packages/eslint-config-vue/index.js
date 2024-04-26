164b901d121c7eb18894d704cc86538e1def5caf
Simon Gaudreau <sgaudreau@exolnet.com>
2024-04-26 13:08:11 -0400

    feat: migrate @exolnet/eslint-config-vue to eslint 9.x flat config

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,24 +1,17 @@
-module.exports = {
-    extends: [
-        require.resolve('@exolnet/eslint-config-base'),
-        'plugin:vue/vue3-recommended',
-    ],
+const configBase = require('@exolnet/eslint-config-base');
+const pluginVue = require('eslint-plugin-vue');
 
-    plugins: ['vue'],
+module.exports = [
+    ...configBase,
+    ...pluginVue.configs['flat/recommended'],
+    {
 
-    rules: {
-        'vue/component-definition-name-casing': 'off',
-        'vue/require-v-for-key': 'off',
-    },
-
-    overrides: [
-        {
-            files: ['*.vue', '**/*.vue'],
-            rules: {
-                indent: 'off',
-                'vue/html-indent': ['warn', 4],
-                'vue/max-attributes-per-line' : 'off',
-            },
+        rules: {
+            'vue/component-definition-name-casing': 'off',
+            'vue/require-v-for-key': 'off',
+            'indent': 'off',
+            'vue/html-indent': ['warn', 4],
+            'vue/max-attributes-per-line' : 'off',
         },
-    ],
-};
+    },
+];
